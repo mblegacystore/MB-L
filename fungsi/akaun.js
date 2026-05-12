094255a575dccc131f7f6a00ae7e938d0f282fc6
Mblegacy store <mblegacystore@gmail.com>
2026-05-12 11:40:04 +0800

    Create akaun.js

---
+++ fungsi/akaun.js
@@ -0,0 +1,16 @@
+async function doLogin() {
+    updateStatus("Menyambung...");
+    try {
+        const auth = await Pi.authenticate(["username", "payments", "wallet_address"]);
+        currentUser = {
+            uid: auth.user.uid,
+            username: auth.user.username,
+            wallet_address: auth.user.wallet_address
+        };
+        updateStatus(currentUser.username);
+        document.getElementById("btn-login").style.display = "none";
+        tryEnablePaymentButtons();
+    } catch (e) {
+        updateStatus("Login gagal");
+    }
+}

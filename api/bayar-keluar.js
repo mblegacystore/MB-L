d42d46ec362d7a7a667f9eff89c6137a7a803324
Mblegacy store <mblegacystore@gmail.com>
2026-05-19 03:11:28 +0800

    Update bayar-keluar.js

--- api/bayar-keluar.js
+++ api/bayar-keluar.js
@@ -1,3 +1,3 @@
-module.exports = async function handler(req, res) {
-    res.status(200).json({ message: "Hello from backend" });
-};
+export default function handler(req, res) {
+    res.status(200).json({ message: "OK" });
+}
